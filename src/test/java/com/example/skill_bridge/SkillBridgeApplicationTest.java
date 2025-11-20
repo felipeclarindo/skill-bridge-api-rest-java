53b02e0f7d09c8b73bcd7978386b92d4bcdb4205
felipeclarindo <fglpc2@gmail.com>
2025-11-20 05:46:15 -0300

    fix(application): Fix error in dependencies, updating tests and adding the application properties.

--- src/test/java/com/example/skill_bridge/SkillBridgeApplicationTest.java
+++ src/test/java/com/example/skill_bridge/SkillBridgeApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SkillBridgeApplicationTests {
+class SkillBridgeApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
